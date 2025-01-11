eff1ddcc17ced7f044fa3b85ae3e8f1eb0eee87e
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-01-11 22:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const ptag = document.getElementById("value");
+const btn = document.getElementById("btn");
+btn.addEventListener("click",logic);
+let value =0;
+function logic(){
+	alert(value);
+	++value;
+	ptag.textContent = value;
+}
